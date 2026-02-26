bb69c6ffc116b85131c68051f052bc23779f0f9c
Valery Buchinsky <valery.buchinsky@gmail.com>
2026-02-26 23:57:55 +0200

    update eslint config to properly allow default project for test files

--- shared/eslint.config.mjs
+++ shared/eslint.config.mjs
@@ -9,7 +9,9 @@ const eslintConfig = [
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ["src/*.test.ts"],
+        },
         tsconfigRootDir: __dirname,
       },
     },
